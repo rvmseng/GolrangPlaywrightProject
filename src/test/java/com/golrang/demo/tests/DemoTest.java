1fc0a6b685f704fceb9ca3580abceac4e763ec17
reza.va <reza.va@IS-01020944.mtnirancell.ir>
2023-02-28 15:48:50 +0330

    test2_with_tracing added

--- src/test/java/com/golrang/demo/tests/DemoTest.java
+++ src/test/java/com/golrang/demo/tests/DemoTest.java
@@ -1,13 +1,17 @@
 package com.golrang.demo.tests;
 
+import java.nio.file.Paths;
+
 import org.testng.annotations.Test;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.BrowserType.LaunchOptions;
 import com.microsoft.playwright.options.BrowserChannel;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Tracing;
 
 public class DemoTest {
 	
@@ -19,9 +23,36 @@ public class DemoTest {
 		lo.setChannel("chrome");
 		Browser browser=Playwright.create().chromium().launch(lo);
 		
-		Page page=browser.newPage();
-		page.navigate("https://www.google.com");
+		BrowserContext context = browser.newContext();
+		Page page=context.newPage();
 		
+		page.navigate("https://www.google.com");
 		System.out.println(page.title());
+	}
+	
+	@Test
+	public void test2_with_tracing() {
+		LaunchOptions lo=new BrowserType.LaunchOptions();
+		lo.setHeadless(false);
+		
+		lo.setChannel("chrome");
+		Browser browser=Playwright.create().chromium().launch(lo);
+		
+		BrowserContext context = browser.newContext();
+		
+		// Start tracing before creating / navigating a page.
+		context.tracing().start(new Tracing.StartOptions()
+											.setScreenshots(true)
+											.setSnapshots(true)
+											.setSources(true));
+		
+		Page page=context.newPage();
+		page.navigate("https://playwright.dev");
+
+		System.out.println(page.title());
+		
+		// Stop tracing and export it into a zip archive.
+		context.tracing().stop(new Tracing.StopOptions()
+											.setPath(Paths.get("trace.zip")));
 	}
 }
