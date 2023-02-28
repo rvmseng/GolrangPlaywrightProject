70829aa89e8c02a15acb73ae4938365f33e6a8c4
reza.va <reza.va@IS-01020944.mtnirancell.ir>
2023-02-28 15:33:02 +0330

    add install bin command

---
+++ Install Binaries.cmd
@@ -0,0 +1,1 @@
+mvn exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D exec.args="install"
--- src/test/java/com/golrang/demo/tests/DemoTest.java
+++ src/test/java/com/golrang/demo/tests/DemoTest.java
@@ -1,19 +1,27 @@
 package com.golrang.demo.tests;
 
-import org.testng.SkipException;
 import org.testng.annotations.Test;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.options.BrowserChannel;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 
 public class DemoTest {
 	
 	@Test
 	public void test1() {
-		throw new SkipException("not implemented");
+		LaunchOptions lo=new BrowserType.LaunchOptions();
+		lo.setHeadless(false);
+		
+		lo.setChannel("chrome");
+		Browser browser=Playwright.create().chromium().launch(lo);
+		
+		Page page=browser.newPage();
+		page.navigate("https://www.google.com");
+		
+		System.out.println(page.title());
 	}
-	
-	@Test
-	public void test2() {
-		// TODO Auto-generated method stub
-
-	}
-
 }
